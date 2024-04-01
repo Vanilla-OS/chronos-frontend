a9d440f670d38c88924239129059260d2d7d2d20
Silva <7Silva>
2024-04-01 12:27:26 -0300

    Added Fastify dependencies and configurations for the server

--- server.js
+++ server.js
@@ -2,18 +2,58 @@ import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import * as express from 'express';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// Fastify
+import Fastify from 'fastify';
+import RateLimit from '@fastify/rate-limit';
+import Helmet from '@fastify/helmet';
+import Cors from '@fastify/cors'
+import Static from '@fastify/static';
 
-const app = express.default();
-const PORT = process.env.PORT || 6090;
+export class Server {
+  app
+  port
+  __filename
+  __dirname
 
-app.use(express.static(resolve(__dirname, "dist")));
+  constructor() {
+    this.app = Fastify({ logger: true })
+    this.port = process.env.PORT || 6090;
+    this.__filename = fileURLToPath(import.meta.url);
+    this.__dirname = dirname(this.__filename);
+  }
+  
+  #middlewares() {
+    return Promise.all([
+      this.app.register(RateLimit, {
+        global: true,
+        max: 100
+      }),
+      this.app.register(Cors),
+      this.app.register(Helmet, {
+        global: true,
+        referrerPolicy: {
+          policy: 'no-referrer'
+        }
+      }),
+      this.app.register(Static, {
+        root: resolve(this.__dirname, 'dist')
+      })
+    ])
+  }
 
-app.get("*", (req, res) => {
-  res.sendFile(resolve(__dirname, "dist", "index.html"));
-});
+  async start() {
+    try {
+      await this.#middlewares()
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+      this.app.listen({ port: this.port }, (err, address) => {
+        if (err) return Promise.reject(err)
+
+        console.log(`Server is running on ${address}`)
+      })
+    } catch(err) {
+      return console.error(err)
+    }
+  }
+}
+
+new Server().start()
