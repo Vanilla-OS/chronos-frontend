872754eabb79d7d890a411a79b22a54fd6fb0ea7
K.B.Dharun Krishna <kbdharunkrishna@gmail.com>
2024-06-26 23:29:04 +0530

    revert: "feat: add proper support for Fastify server"
    This reverts commit 4bc2adca3787a55a1ac564866bfca5422f78c5c4.

--- server.js
+++ server.js
@@ -1,53 +1,19 @@
-import Fastify from 'fastify';
-import RateLimit from '@fastify/rate-limit';
-import Cors from '@fastify/cors';
-import Helmet from '@fastify/helmet';
-import Static from '@fastify/static';
-import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import * as express from 'express';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = Fastify({ logger: true });
+const app = express.default();
+const PORT = process.env.PORT || 6090;
 
-class Server {
-  constructor(app) {
-    this.app = app;
-    this.__dirname = __dirname;
-  }
+app.use(express.static(resolve(__dirname, "dist")));
 
-  #middlewares() {
-    return Promise.all([
-      this.app.register(RateLimit, {
-        max: 100, // Max number of requests allowed in the time window
-        timeWindow: '1 minute'
-      }),
-      this.app.register(Cors),
-      this.app.register(Helmet, {
-        global: true,
-        referrerPolicy: {
-          policy: 'no-referrer'
-        }
-      }),
-      this.app.register(Static, {
-        root: resolve(this.__dirname, 'dist'),
-        prefix: '/',
-      })
-    ]);
-  }
+app.get("*", (req, res) => {
+  res.sendFile(resolve(__dirname, "dist", "index.html"));
+});
 
-  async start(PORT) {
-    await this.#middlewares();
-    this.app.listen({ port: PORT, host: '0.0.0.0' })
-      .then((address) => console.log(`Server is running on ${address}`))
-      .catch(err => {
-        this.app.log.error(err);
-        process.exit(1);
-      });
-  }
-}
-
-const server = new Server(app);
-const PORT = process.env.PORT || 6090; // Use the PORT environment variable, default to 6090 if not set
-server.start(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
