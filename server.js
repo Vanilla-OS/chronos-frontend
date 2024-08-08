de75ac80e1ad556bc786a8abbe64e8b0d9dd43f4
Gabriel Cozma <me@gabs.eu.org>
2024-08-08 12:14:20 +0300

    feat: migrate server to fastify

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
-import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
-import * as express from 'express';
+
+import fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import fastifyRateLimit from '@fastify/rate-limit';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express.default();
+const app = fastify();
 const PORT = process.env.PORT || 6090;
 
-app.use(express.static(resolve(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(resolve(__dirname, "dist", "index.html"));
+app.register(fastifyStatic, {
+  root: resolve(__dirname, 'dist')
 });
 
-app.listen(PORT, () => {
+app.register(fastifyRateLimit, {
+  max: 300,
+  timeWindow: '1 minute'
+});
+
+app.get('/', (req, reply) => {
+  reply.sendFile('index.html');
+});
+
+app.listen({ port: PORT }, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
