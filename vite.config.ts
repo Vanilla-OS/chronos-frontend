80b18ae0249b21d3e040872ab754d30fff8eea8f
Gabriel Cozma <me@gxbs.dev>
2025-02-11 17:54:15 +0200

    cleanup: update tailwind to latest major version
    Adds support for Tailwind v4, without requring postcss at all

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
