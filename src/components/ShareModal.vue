0a8ea4faece404f8be2dd8614af6623e3a4d930e
Gabriel Cozma <me@gxbs.dev>
2025-02-28 16:06:43 +0200

    fix(#159): bad contrast for share on light mode

--- src/components/ShareModal.vue
+++ src/components/ShareModal.vue
@@ -3,11 +3,11 @@
     class="flex fixed top-0 left-0 justify-center items-center z-40 size-full"
   >
     <div
-      class="flex absolute top-0 left-0 size-full bg-gray-400 dark:bg-black opacity-50 backdrop-blur-xl z-40"
+      class="flex absolute top-0 left-0 size-full bg-gray-100 dark:bg-black backdrop-blur-xl z-40"
       @click="$emit('close')"
     ></div>
     <div
-      class="relative shadow-lg rounded-xl bg-gray-100 dark:bg-gray-900 w-[30%] h-max z-50"
+      class="relative shadow-lg rounded-xl bg-white dark:bg-gray-900 w-[30%] h-max z-50"
     >
       <div class="grid gap-4 p-6">
         <div class="flex justify-between items-center">
@@ -25,54 +25,54 @@
           <a
             :href="mastodonLink"
             target="_blank"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600"
           >
             <i class="fa-brands fa-mastodon text-gray-900 dark:text-white"></i>
           </a>
           <a
             :href="twitterLink"
             target="_blank"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600"
           >
             <i class="fa-brands fa-twitter text-gray-900 dark:text-white"></i>
           </a>
           <a
             :href="telegramLink"
             target="_blank"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600"
           >
             <i class="fa-brands fa-telegram text-gray-900 dark:text-white"></i>
           </a>
           <a
             :href="facebookLink"
             target="_blank"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600"
           >
             <i class="fa-brands fa-facebook text-gray-900 dark:text-white"></i>
           </a>
           <a
             :href="whatsappLink"
             target="_blank"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600"
           >
             <i class="fa-brands fa-whatsapp text-gray-900 dark:text-white"></i>
           </a>
           <a
             :href="linkedinLink"
             target="_blank"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600"
           >
             <i class="fa-brands fa-linkedin text-gray-900 dark:text-white"></i>
           </a>
           <div
             @click="sendEmail"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600 cursor-pointer"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600 cursor-pointer"
           >
             <i class="fa-regular fa-envelope text-gray-900 dark:text-white"></i>
           </div>
           <div
             @click="copyLink"
-            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-600 dark:hover:bg-gray-600 col-span-3 cursor-pointer"
+            class="p-4 bg-gray-100 dark:bg-gray-800 dark:bg-opacity-50 rounded-full flex items-center justify-center hover:bg-gray-200 dark:hover:bg-gray-600 col-span-3 cursor-pointer"
           >
             <i class="fa-regular fa-copy text-gray-900 dark:text-white"></i>
             <span class="ml-2 text-gray-900 dark:text-white">
