1c7fae8ad546e347cd67f48eff03246d06bb6b2e
Gabriel Cozma <me@gxbs.dev>
2026-01-16 23:47:09 +0200

    feat: redesign chronos ui

--- src/views/CollectionView.vue
+++ src/views/CollectionView.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-zinc-100 dark:bg-zinc-800 text-black dark:text-zinc-200 text-center py-8">
+  <section class="text-black dark:text-zinc-200 text-center py-8 mt-16.25">
     <div class="container mx-auto px-4 max-w-6xl">
       <h1 class="text-3xl font-bold">{{ activeCollection!.title }}</h1>
       <p class="mt-4">{{ activeCollection!.description }}</p>
@@ -15,7 +15,7 @@
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6" v-if="stories">
       <article v-for="story in stories" :key="story.Name"
-        class="flex flex-col bg-white dark:bg-zinc-800 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out p-5">
+               class="flex flex-col bg-white dark:bg-zinc-800 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out p-5">
         <router-link :to="`/${collectionName}/${chronosStore.prefLang}/${story.StartSlug}`" class="flex flex-col gap-2">
           <h4 class="text-xl font-semibold text-black dark:text-zinc-200">
             {{ story.Name }}
@@ -34,7 +34,7 @@
 
     <!-- Toolbox -->
     <aside
-      class="bg-white dark:bg-zinc-800 p-4 rounded-lg shadow md:sticky top-2 flex flex-row gap-4 mt-2 items-center">
+      class="border dark:border-zinc-800 border-zinc-300 rounded-xl p-4 md:sticky top-2 flex flex-row gap-4 mt-2 items-center">
       <router-link class="text-blue-600 dark:text-blue-400 flex items-center mr-auto" to="/">
         <i class="material-symbols-outlined">arrow_back</i>
         <span class="hidden sm:inline">Back to collections</span>
@@ -42,13 +42,13 @@
 
       <div class="flex flex-row items-center gap-2" v-if="articlesResponse.tags">
         <label for="tag-filter"
-          class="whitespace-nowrap text-sm font-medium text-zinc-700 dark:text-zinc-300">Tags:</label>
+               class="whitespace-nowrap text-sm font-medium text-zinc-700 dark:text-zinc-300">Tags:</label>
         <CustomSelect :options="articlesResponse.tags" v-model="selectedTags" multiple placeholder="Select tags" />
       </div>
 
       <div class="flex flex-row items-center gap-2">
         <label for="date-filter"
-          class="whitespace-nowrap text-sm font-medium text-zinc-700 dark:text-zinc-300">Order:</label>
+               class="whitespace-nowrap text-sm font-medium text-zinc-700 dark:text-zinc-300">Order:</label>
         <CustomSelect :options="['Recent', 'Oldest']" v-model="selectedDate" placeholder="Select date" />
       </div>
     </aside>
@@ -61,7 +61,7 @@
     <div class="flex-1" v-if="articlesResponse?.articles">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
         <article v-for="article in filteredArticles" :key="article.Slug"
-          class="flex flex-col bg-white dark:bg-zinc-800 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out p-5">
+                 class="border dark:border-zinc-800 border-zinc-300 hover:border-zinc-400 hover:dark:border-zinc-700 rounded-xl overflow-hidden transition duration-300 ease-in-out flex flex-col p-8 gap-4">
           <div class="flex-1">
             <h4 class="text-xl font-semibold mb-2 text-black dark:text-zinc-200">
               {{ article.Title }}
@@ -69,13 +69,13 @@
             <p class="text-zinc-700 dark:text-zinc-300 mb-4">{{ article.Description }}</p>
             <div class="flex flex-wrap gap-2 mt-2 mb-4">
               <span v-for="tag in article.Tags" :key="tag" @click="setTag(tag)"
-                class="inline-block bg-blue-100 dark:bg-zinc-800 text-blue-800 dark:text-blue-200 rounded-full px-3 py-1 text-xs font-medium cursor-pointer">
+                    class="inline-block bg-zinc-100 dark:bg-zinc-800 text-black dark:text-white/50 rounded-full px-3 py-1 text-xs font-medium cursor-pointer">
                 {{ tag }}
               </span>
             </div>
           </div>
           <router-link :to="`/${collectionName}/${chronosStore.prefLang}/${article.Slug}`"
-            class="mt-auto inline-flex items-center text-lg text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300 font-semibold transition-colors duration-150 ease-in-out">
+                       class="mt-auto inline-flex items-center text-lg text-black dark:text-white font-semibold transition-colors duration-150 ease-in-out">
             Read the article
             <i class="material-symbols-outlined ml-2">arrow_forward</i>
           </router-link>
@@ -98,7 +98,7 @@ import CustomSelect from "../components/CustomSelect.vue";
 export default defineComponent({
   name: "CollectionView",
   components: {
-    CustomSelect,
+    CustomSelect
   },
   data() {
     return {
@@ -125,9 +125,9 @@ export default defineComponent({
         });
       }
 
-      if (this.selectedDate[0] === 'Recent') {
+      if (this.selectedDate[0] === "Recent") {
         filtered = filtered.sort((a, b) => new Date(b.PublicationDate).getTime() - new Date(a.PublicationDate).getTime());
-      } else if (this.selectedDate[0] === 'Oldest') {
+      } else if (this.selectedDate[0] === "Oldest") {
         filtered = filtered.sort((a, b) => new Date(a.PublicationDate).getTime() - new Date(b.PublicationDate).getTime());
       }
 
@@ -169,7 +169,7 @@ export default defineComponent({
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:title", content: this.activeCollection!.title },
         { name: "twitter:description", content: this.activeCollection!.description },
-        { name: "twitter:image", content: this.chronosConfig!.logoUrl },
+        { name: "twitter:image", content: this.chronosConfig!.logoUrl }
       ];
       for (const metaTag of metaTags) {
         const meta = document.createElement("meta");
@@ -179,8 +179,8 @@ export default defineComponent({
       }
     },
     setTag(tag: string) {
-      this.selectedTags = [tag as never]
+      this.selectedTags = [tag as never];
     }
-  },
+  }
 });
 </script>
